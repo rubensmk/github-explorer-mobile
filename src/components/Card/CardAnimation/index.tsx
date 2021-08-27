e8d2b268c42af1ce4fae1420ee74c83b6dd954ea
rubensmk <rubenskishimoto@gmail.com>
2021-08-27 11:45:05 -0300

    feat:Finished :sparkles:

--- src/components/Card/CardAnimation/index.tsx
+++ src/components/Card/CardAnimation/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useWindowDimensions, ViewProps } from 'react-native';
 import {
+  Extrapolate,
+  interpolate,
   useAnimatedStyle,
   useSharedValue,
   withTiming
@@ -19,15 +21,18 @@ export function CardAnimation({ children, ...rest }: CardAnimationProps) {
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
-      // TODO - setup animated style
+      opacity: interpolate(cardOpacity.value, [0, 0.4, 1], [0, 0.4, 1]),
+      transform: [
+        {
+          translateX: interpolate(cardOffset.value, [200, 0], [200, 0], Extrapolate.CLAMP)
+        }
+      ]
     }
   })
 
   useEffect(() => {
-    /**
-     * TODO - setup cardOpacity.value and cardOffset.value with
-     * withTiming()
-     */
+    cardOpacity.value = withTiming(1, { duration: 1000 });
+    cardOffset.value = withTiming(0, { duration: 1000 });
   }, []);
 
   return (
